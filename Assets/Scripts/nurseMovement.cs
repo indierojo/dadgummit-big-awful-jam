e842b4f1533161226390ae3227524b9b4ed28a24
CowFu <spaehanson@gmail.com>
2016-07-05 22:39:22 -0500

    Add nurse prefab
    nurse prefab has a copy of playerMovement script called nurseMovement
    it's identical so input makes her move, this i just a placeholder until
    we can get a proper AI movement

---
+++ Assets/Scripts/nurseMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nurseMovement : MonoBehaviour {
+
+    Animator anim;
+    public float speed = 0;
+    private float movex = 0f;
+    private float movey = 0f;
+    private bool isWalking = false;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movex = Input.GetAxisRaw("Horizontal");
+        movey = Input.GetAxisRaw("Vertical");
+
+        isWalking = (Mathf.Abs(movex) + Mathf.Abs(movey) > 0);
+
+        anim.SetBool("isWalking", isWalking);
+        if (isWalking)
+        {
+            anim.SetFloat("x", movex);
+            anim.SetFloat("y", movey);
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * speed, movey * speed);
+
+    }
+}
